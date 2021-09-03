6ec510a8f10a0cbe57b968c5bb460233b03791ec
hahn <hahn2017@my.yorku.ca>
2021-09-02 22:45:51 -0400

    LetterCombinationsOfAPhoneNumber

---
+++ Medium/17.LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static List<String> letterCombinations(String digits) {
+        if(digits.equals("")) return List.of();
+        List<String> result = new ArrayList<String>();
+        String[] map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        helper(result, map, "", digits, 0);
+        return result;
+    }
+
+    public static void helper(List<String> result, String[] map, String str, String digits, int index){
+        int digit = digits.charAt(index) -'0';
+        String tempStr;
+        for(int i = 0; i < map[digit - 2].length(); i++){
+            tempStr = str + map[digit - 2].charAt(i);
+            if(index == digits.length() - 1){
+                result.add(tempStr);
+                continue;
+            }
+            helper(result, map, tempStr, digits, index +  1);
+        }
+        return;
+    }
+}
