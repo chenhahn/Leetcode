2a0800b2cc9c87a835553d548b20d65f299906b3
hahn <hahn2017@my.yorku.ca>
2021-10-18 22:46:17 -0400

    53. Maximum Subarray

---
+++ Easy/53.MaximumSubarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, max = Integer.MIN_VALUE;
+        for(int num: nums){
+            sum += num;
+            max = Math.max(sum, max);
+            sum = sum < 0 ? 0 : sum;
+        }
+        return max;
+    }
+}
