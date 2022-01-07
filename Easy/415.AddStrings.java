9f2ac51bd7ff61ae7826fd0c7141cd6844b7c0eb
hahn2017 <chenhahn2017@gmail.com>
2022-01-06 21:13:45 -0500

    415. Add Strings

---
+++ Easy/415.AddStrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int n1 = (i >= 0) ? (num1.charAt(i) - '0') : 0;
+            int n2 = (j >= 0) ? (num2.charAt(j) - '0') : 0;
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+            sb.append(sum % 10);
+            i--;
+            j--;
+        }
+        
+        if (carry > 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
+// Time complexity: O(N) 	N -> the max length of two strings
+// Space complexity: O(N)
