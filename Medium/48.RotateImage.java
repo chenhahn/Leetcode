f74e3ff62b544bb4bf33b375829e7342a1627cfc
hahn2017 <chenhahn2017@gmail.com>
2022-01-23 11:20:55 -0500

    48. Rotate Image

---
+++ Medium/48.RotateImage.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-i-1][j];
+                matrix[n-i-1][j] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
+
+// Time complexity: O(n^2)	n*n matrix, reverse vertically O(n^2); reverse on diagonal O(n^2)
+// Space complexity: O(1)
