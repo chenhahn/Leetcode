d958a25f0ee5726213f1586f76c28909f10f6836
hahn2017 <chenhahn2017@gmail.com>
2022-02-22 23:02:55 -0500

    88. Merge Sorted Array

---
+++ Easy/88. Merge Sorted Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index1 = m - 1,  index2 = n - 1;
+        int i = m + n - 1;
+        while (index1 >= 0 && index2 >= 0) {
+            nums1[i--] = (nums1[index1] > nums2[index2]) ? nums1[index1--] : nums2[index2--];
+        }
+        while (index2 >= 0) {
+            nums1[i--] = nums2[index2--];
+        }
+    }
+}
